18a62f02cf7a47efc6ac95defc0168ea5f6192d1
dFaery <137483979+dFaery@users.noreply.github.com>
2026-05-06 15:51:22 +0700

    feat: consultations pages (haven't start create chat feature yet)
    auth -> redirect to chat
    guest -> modal login
    still havent applied filter by doctors specialties

--- project/routes/role/member.php
+++ project/routes/role/member.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\ChatController;
 use App\Http\Controllers\DoctorController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:member'])->group(function () {
     #region API ROUTES (TEMPORARY)    
+    Route::get('/chat', [ChatController::class, 'index'])->name('member.chat');
     #endregion
 });
